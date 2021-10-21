d77d35b07a41cf13b28aab5cf7f2a5f508a34f5d
Ksenia Mochalova <kmochalova@sfedu.ru>
2021-10-21 15:02:58 +0300

    Add function 'SecondsInMinute' in 'DateTime.pas' file

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,12 @@
-﻿function IsLeapYear(year);
+﻿function IsLeapYear(year: integer): boolean;
 begin
   if ((year mod 4 == 0) and ((year mod 100 == 0) or (year mod 400 == 0)))
   then result := True else result := False;
+end;
+
+function SecondsInMinute(x: integer): integer;
+begin
+  result := x*60;
 end;
 
 begin
