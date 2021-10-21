ec6e2bc70993cd71d23ebd08b42bb6afb8745d15
Ksenia Mochalova <kmochalova@sfedu.ru>
2021-10-21 14:59:51 +0300

    Add function 'IsLeapYear' in 'DateTime.pas'

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,9 @@
-﻿begin
+﻿function IsLeapYear(year);
+begin
+  if ((year mod 4 == 0) and ((year mod 100 == 0) or (year mod 400 == 0)))
+  then result := True else result := False;
+end;
+
+begin
   
 end.
